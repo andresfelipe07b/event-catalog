c70d654fdd4d82bd3675e45250348ccaef2c468e
Andresf07b <bolanosandres43@gmail.com>
2025-11-17 19:32:01 -0500

    Initial commit

---
+++ src/main/java/org/riwi/eventcatalog/EventCatalogApplication.java
@@ -0,0 +1,13 @@
+package org.riwi.eventcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/riwi/eventcatalog/EventCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.riwi.eventcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
